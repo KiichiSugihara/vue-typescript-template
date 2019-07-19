647d7256b5361fc9d89c7f9af96436efc7ad75a4
Kiichi_Sugihara <34018277+KiichiSugihara@users.noreply.github.com>
2019-07-19 12:42:04 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <div>hoge</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
